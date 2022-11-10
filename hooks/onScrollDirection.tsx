50962f646bf308795dbfd56c3a8ba3dbb5ffa9fd
lorepieri <lorenzopieriantidote@gmail.com>
2022-11-10 13:05:49 +0100

    fixed ts annoying msgs

--- hooks/onScrollDirection.tsx
+++ hooks/onScrollDirection.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 function useScrollDirection() {
-  const [scrollDirection, setScrollDirection] = useState(null);
+  const [scrollDirection, setScrollDirection] = useState<any>(null);
 
   useEffect(() => {
     let lastScrollY = window.pageYOffset;
