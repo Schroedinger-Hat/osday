e510585e3bc8d9fbfa1be4740cd1a9d0518225de
404answernotfound <404answernotfound.public@gmail.com>
2022-11-15 15:43:41 +0100

    fixed absolute containers

--- components/Main.tsx
+++ components/Main.tsx
@@ -7,9 +7,6 @@ export default function Main() {
   return (
     <div className="container">
       <Hero mainCta={{ text: 'Call for Papers', link: 'https://sessionize.com/opensourceday23' }} secondaryCta={{ text: 'Send us an email', link: 'mailto:osday@schrodinger-hat.it' }} />
-      {/* <div className="image">
-        <Image className="img" alt="background" fill={true} src="/bg.jpg" />
-      </div> */}
       <section className="talks_2021">
         <h2>Take a look at the first edition</h2>
         <div className="talks_container">
