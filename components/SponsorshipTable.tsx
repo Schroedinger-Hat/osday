5cf32f77e70099a0dff7a85eaae80f5c6c5c6df1
404answernotfound <404answernotfound.public@gmail.com>
2022-12-12 19:35:00 +0100

    feat/sponsortable/added new sponsorship table

--- components/SponsorshipTable.tsx
+++ components/SponsorshipTable.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { useTranslations } from 'next-intl';
+import React, { ReactNode } from 'react';
+import SponsorshipTier from './SponsorshipTier';
 
 type TPremiums = {
   type: string;
   text: string;
   has: {
+    community: boolean;
+    brand: boolean;
     silver: boolean;
     gold: boolean;
     diamond: boolean;
@@ -12,11 +16,19 @@ type TPremiums = {
   image?: string;
   url?: string;
 };
+
+/*
+ * Logo on streaming video
+ * Stand in expo area
+ * Tech talk */
+
 const premiums: TPremiums[] = [
   {
     type: 'thanksYou',
     text: 'Thank you',
     has: {
+      community: true,
+      brand: true,
       silver: false,
       gold: true,
       diamond: true,
@@ -27,6 +39,8 @@ const premiums: TPremiums[] = [
     type: 'socialPost',
     text: 'Social post with logo',
     has: {
+      community: true,
+      brand: true,
       silver: true,
       gold: true,
       diamond: false,
@@ -34,19 +48,11 @@ const premiums: TPremiums[] = [
     }
   },
   {
-    type: 'rollupLogo',
-    text: 'Logo on Rollup',
-    has: {
-      silver: false,
-      gold: true,
-      diamond: true,
-      platinum: true
-    }
-  },
-  {
     type: 'websiteLogo',
     text: 'Logo on website',
     has: {
+      community: true,
+      brand: true,
       silver: false,
       gold: false,
       diamond: true,
@@ -57,7 +63,81 @@ const premiums: TPremiums[] = [
     type: 'posterLogo',
     text: 'Logo on poster',
     has: {
+      community: false,
+      brand: false,
       silver: true,
+      gold: true,
+      diamond: true,
+      platinum: true
+    }
+  },
+  {
+    type: 'socialAwareness',
+    text: 'Social Awareness',
+    has: {
+      community: true,
+      brand: true,
+      silver: true,
+      gold: true,
+      diamond: true,
+      platinum: true
+    }
+  },
+  {
+    type: 'attendees',
+    text: "Attendee's list",
+    has: {
+      community: false,
+      brand: false,
+      silver: false,
+      gold: false,
+      diamond: true,
+      platinum: false
+    }
+  },
+  {
+    type: 'rollUp',
+    text: 'Rollup Logo Print',
+    has: {
+      community: false,
+      brand: false,
+      silver: true,
+      gold: true,
+      diamond: true,
+      platinum: true
+    }
+  },
+  {
+    type: 'streamingVideo',
+    text: 'Logo in streaming',
+    has: {
+      community: false,
+      brand: false,
+      silver: false,
+      gold: true,
+      diamond: true,
+      platinum: true
+    }
+  },
+  {
+    type: 'standExpoArea',
+    text: 'Stand in Expo Area',
+    has: {
+      community: false,
+      brand: false,
+      silver: false,
+      gold: false,
+      diamond: true,
+      platinum: true
+    }
+  },
+  {
+    type: 'techTalk',
+    text: 'Tech talk (non commercial)',
+    has: {
+      community: false,
+      brand: false,
+      silver: false,
       gold: false,
       diamond: false,
       platinum: true
@@ -66,27 +146,35 @@ const premiums: TPremiums[] = [
 ];
 
 export default function SponsorshipTable() {
+  const t = useTranslations('Sponsor');
+
   return (
     <>
       <div className="sponsortable">
         <div className="sponsortable_tiers_mobile">
-            <div className="sponsortable_tiers_card">
-              <span className="">silver</span>
-            </div>
-            <div className="sponsortable_tiers_card">
-              <span className="">gold</span>
-            </div>
-            <div className="sponsortable_tiers_card">
-              <span className="">diamond</span>
-            </div>
-            <div className="sponsortable_tiers_card">
-              <span className="">platinum</span>
-            </div>
+          <div className="sponsortable_tiers_card">
+            <span className="">silver</span>
+          </div>
+          <div className="sponsortable_tiers_card">
+            <span className="">gold</span>
+          </div>
+          <div className="sponsortable_tiers_card">
+            <span className="">diamond</span>
+          </div>
+          <div className="sponsortable_tiers_card">
+            <span className="">platinum</span>
+          </div>
         </div>
         <table>
           <thead className="sponsortable_tiers">
             <tr>
               <th></th>
+              <th className="sponsortable_tiers_card">
+                <span className="">community</span>
+              </th>
+              <th className="sponsortable_tiers_card">
+                <span className="">brand</span>
+              </th>
               <th className="sponsortable_tiers_card">
                 <span className="">silver</span>
               </th>
@@ -106,15 +194,80 @@ export default function SponsorshipTable() {
               return (
                 <tr key={premium.type}>
                   <td>{premium.text}</td>
-                  <td>{premium.has.silver ? '\u2714' : '\u2717'}</td>
-                  <td>{premium.has.gold ? '\u2714' : '\u2717'}</td>
-                  <td>{premium.has.diamond ? '\u2714' : '\u2717'}</td>
-                  <td>{premium.has.platinum ? '\u2714' : '\u2717'}</td>
+                  <td className="sponsortable_onlyDesktop">
+                    {premium.has.community ? (
+                      <span className="confirmed_tier">&#10004;</span>
+                    ) : (
+                      <span>&#10007;</span>
+                    )}
+                  </td>
+                  <td className="sponsortable_onlyDesktop">
+                    {premium.has.brand ? (
+                      <span className="confirmed_tier">&#10004;</span>
+                    ) : (
+                      <span>&#10007;</span>
+                    )}
+                  </td>
+                  <td>
+                    {premium.has.silver ? (
+                      <span className="confirmed_tier">&#10004;</span>
+                    ) : (
+                      <span>&#10007;</span>
+                    )}
+                  </td>
+                  <td>
+                    {premium.has.gold ? (
+                      <span className="confirmed_tier">&#10004;</span>
+                    ) : (
+                      <span>&#10007;</span>
+                    )}
+                  </td>
+                  <td>
+                    {premium.has.diamond ? (
+                      <span className="confirmed_tier">&#10004;</span>
+                    ) : (
+                      <span>&#10007;</span>
+                    )}
+                  </td>
+                  <td>
+                    {premium.has.platinum ? (
+                      <span className="confirmed_tier">&#10004;</span>
+                    ) : (
+                      <span>&#10007;</span>
+                    )}
+                  </td>
                 </tr>
               );
             })}
           </tbody>
         </table>
+
+        <section className="sponsors_2022">
+          <SponsorshipTier
+            title={t('community')}
+            card_color="community"
+            offer={t('community_offer')}
+            emailLink={t.rich('send_email', {
+              email: (children: ReactNode) => (
+                <a href="mailto:osday@schrodinger-hat.it?subject=OSDay 2023, Community Partner">
+                  <u>{children}</u>
+                </a>
+              )
+            })}
+          />
+          <SponsorshipTier
+            title={t('media')}
+            card_color="media"
+            offer={t('media_offer')}
+            emailLink={t.rich('send_email', {
+              email: (children: ReactNode) => (
+                <a href="mailto:osday@schrodinger-hat.it?subject=OSDay 2023, Media Partner">
+                  <u>{children}</u>
+                </a>
+              )
+            })}
+          />
+        </section>
       </div>
     </>
   );
