155bae20200c615d0f40b5a491ff8d242a346a54
Nicola Puppa <72783243+nicpuppa@users.noreply.github.com>
2023-01-17 14:49:46 +0000

    feat: move linkedin near other social

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,14 +22,14 @@ export default function Footer() {
             <a href="https://www.instagram.com/schrodinger_hat/" target="_blank" rel="noreferrer" title="Instagram">
               <Image width={30} height={30} src="/icons/instagram.svg" alt="Instagram" />
             </a>
+            <a href="https://www.linkedin.com/company/schrodinger-hat/" title="Linkedin" target="_blank" rel="noreferrer">
+              <Image width={28} height={28} src="/icons/linkedin.svg" alt="Linkedin" />
+            </a>
             <a href="https://github.com/Schrodinger-Hat/osday-2023/" target="_blank" rel="noreferrer" title="Github">
               <Image width={30} height={30} src="/icons/github.svg" alt="Github" />
             </a>
             <a href="https://opencollective.com/schrodinger-hat/contribute" title="OpenCollective" target="_blank" rel="noreferrer">
               <Image width={30} height={30} src="/icons/opencollective.svg" alt="OpenCollective" />
-            </a>
-            <a href="https://www.linkedin.com/company/schrodinger-hat/" title="Linkedin" target="_blank" rel="noreferrer">
-              <Image width={28} height={28} src="/icons/linkedin.svg" alt="Linkedin" />
             </a>
           </div>
           <div className="footer-center">
