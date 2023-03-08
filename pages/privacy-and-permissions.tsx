7de5f2e1bfcf7e0e05f927d911b18824bd3b72b3
404answernotfound <404answernotfound.public@gmail.com>
2023-03-08 18:15:46 +0100

    Added privacy and permission policy for event

--- pages/privacy-and-permissions.tsx
+++ pages/privacy-and-permissions.tsx
@@ -25,8 +25,8 @@ export default function CodeOfConduct() {
           <h3>Oggetto del trattamento</h3>
 
           <p>
-            Informativa ai sensi dell’art. 13 del Regolamento UE 679/2016. Ai
-            sensi dell’art. 13 del Regolamento UE 679/2016, recante disposizioni
+            Informativa ai sensi dell&lsquo;art. 13 del Regolamento UE 679/2016. Ai
+            sensi dell&lsquo;art. 13 del Regolamento UE 679/2016, recante disposizioni
             a tutela delle persone e di altri soggetti rispetto al trattamento
             dei dati personali, desideriamo informarLa che i dati personali da
             Lei forniti formeranno oggetto di trattamento nel rispetto della
@@ -36,10 +36,10 @@ export default function CodeOfConduct() {
           <h3>Titolare</h3>
 
           <p>
-            Titolare del trattamento è l’ Associazione non riconosciuta
+            Titolare del trattamento è l&lsquo; Associazione non riconosciuta
             Schrodinger Hat, in persona dei soci Michele Lombardi, Gabriele
             Puliti, Nicola Puppa, Davide Imola, Lorenzo Pieri (in seguito,
-            “Titolare”). L’ Associazione non riconosciuta Schrodinger Hat,
+            “Titolare”). L&lsquo; Associazione non riconosciuta Schrodinger Hat,
             provvede a svolgere attività didattica in ambito di progettazione
             software e di organizzazione di corsi formativi e di istruzione.
           </p>
@@ -51,7 +51,7 @@ export default function CodeOfConduct() {
             trattati i Vostri dati comuni (a titolo esemplificativo e non
             esaustivo: nome, cognome, luogo di nascita, codice fiscale, luogo di
             residenza, ecc.). Questi dati saranno trattati dal titolare del
-            trattamento per espletamento dell’attività professionale richiesta
+            trattamento per espletamento dell&lsquo;attività professionale richiesta
             nel rispetto della normativa vigente in materia di protezione dei
             dati.
           </p>
@@ -59,10 +59,10 @@ export default function CodeOfConduct() {
           <h3>Dati personali di Suoi familiari</h3>
 
           <p>
-            Per la gestione dell’incarico professionale ricevuto il Titolare
+            Per la gestione dell&lsquo;incarico professionale ricevuto il Titolare
             potrà, altresì, trattare dati personali di Vostri familiari al fine
-            di procedere all’emissione della corretta intestazione della fattura
-            per il pagamento della prestazione erogata dall’azienda. In tal
+            di procedere all&lsquo;emissione della corretta intestazione della fattura
+            per il pagamento della prestazione erogata dall&lsquo;azienda. In tal
             senso Vi impegnate ad informare i Vostri familiari di tali
             trattamenti, anche fornendo loro la presente informativa.
           </p>
@@ -70,7 +70,7 @@ export default function CodeOfConduct() {
           <h3>Finalità del trattamento</h3>
 
           <p>
-            l’Associazione non riconosciuta Schrodinger Hat, raccoglie e tratta
+            l&lsquo;Associazione non riconosciuta Schrodinger Hat, raccoglie e tratta
             i dati personali di:
             <ul>
               <li>Clienti e potenziali clienti </li>
@@ -90,7 +90,7 @@ export default function CodeOfConduct() {
               </li>
 
               <li>
-                adempiere agli obblighi di legge che gravano sull’azienda in
+                adempiere agli obblighi di legge che gravano sull&lsquo;azienda in
                 forza di norme civilistiche, fiscali e contabili (tra cui,
                 obbligo di conservazione delle scritture contabili);
               </li>
@@ -104,10 +104,10 @@ export default function CodeOfConduct() {
             trattamento:
             <ul>
               <li>
-                sia necessario all’esecuzione di un contratto di cui siete parte
-                o nel caso in cui vi sia il consenso dell’interessato ex D.Lgs.
+                sia necessario all&lsquo;esecuzione di un contratto di cui siete parte
+                o nel caso in cui vi sia il consenso dell&lsquo;interessato ex D.Lgs.
                 81/2008. Il conferimento dei Vostri dati è obbligatorio per
-                poter erogare i servizi dell’azienda e l'eventuale rifiuto di
+                poter erogare i servizi dell&lsquo;azienda e l&lsquo;eventuale rifiuto di
                 fornire tali dati potrebbe comportare la mancata o parziale
                 prosecuzione del rapporto;
               </li>
@@ -122,7 +122,7 @@ export default function CodeOfConduct() {
 
           <p>
             Il trattamento dei dati è realizzato per mezzo delle operazioni
-            indicate all’art. 4 n. 2) GDPR e precisamente: raccolta,
+            indicate all&lsquo;art. 4 n. 2 GDPR e precisamente: raccolta,
             registrazione, organizzazione, conservazione, consultazione,
             elaborazione, modificazione, selezione, estrazione, raffronto,
             utilizzo, interconnessione, blocco, comunicazione, cancellazione e
@@ -158,7 +158,7 @@ export default function CodeOfConduct() {
             non saranno oggetto di comunicazione senza Vostro esplicito
             consenso, salvo le comunicazioni necessarie che possono comportare
             il trasferimento di dati ad enti pubblici, a consulenti o ad altri
-            soggetti per l’adempimento degli obblighi di legge
+            soggetti per l&lsquo;adempimento degli obblighi di legge
           </p>
 
           <h3>
@@ -168,8 +168,8 @@ export default function CodeOfConduct() {
           <p>
             Il conferimento dei vostri dati per le finalità sopra indicate è
             obbligatorio in quanto necessario per la conclusione e/o
-            l’esecuzione di obblighi contrattuali e/o legali, la mancata
-            comunicazione dei dati comporta pertanto l’impossibilità di
+            l&lsquo;esecuzione di obblighi contrattuali e/o legali, la mancata
+            comunicazione dei dati comporta pertanto l&lsquo;impossibilità di
             adempiere a tali obblighi
           </p>
 
@@ -178,7 +178,7 @@ export default function CodeOfConduct() {
           <p>
             Senza il Vostro consenso, i Dati potranno essere comunicati a
             soggetti terzi nominati responsabili del trattamento ai sensi
-            dell’articolo 28 del Regolamento e in particolare a istituti
+            dell&lsquo;articolo 28 del Regolamento e in particolare a istituti
             bancari, a società attive nel campo assicurativo, a fornitori di
             servizi, persone fisiche e/o giuridiche che erogano il servizio e/o
             che gestiscono il bene da Voi richiesto al Titolare, consulenti e a
@@ -188,7 +188,7 @@ export default function CodeOfConduct() {
             condivisi con autorità, enti e/o soggetti a cui vadano comunicati in
             forza di disposizioni di legge od ordini di autorità e non saranno
             diffusi né vi è alcuna intenzione da parte del Titolare di
-            trasferire i dati in un paese terzo o a un’organizzazione
+            trasferire i dati in un paese terzo o a un&lsquo;organizzazione
             internazionale
           </p>
 
@@ -198,7 +198,7 @@ export default function CodeOfConduct() {
             I dati personali conferiti posso essere soggetti a diffusione e/o ad
             un processo decisionale interamente automatizzato, ivi compresa la
             profilazione, inoltre potranno essere diffusi e trasferiti anche ad
-            un paese terzo o a un’organizzazione internazionale.
+            un paese terzo o a un&lsquo;organizzazione internazionale.
           </p>
 
           <h3> Diritti</h3>
@@ -207,22 +207,22 @@ export default function CodeOfConduct() {
             Tra i diritti a Voi riconosciuti dal GDPR rientrano quelli di:
             <ul>
               <li>
-                chiedere al Titolare l'accesso ai Vostri i dati personali ed
+                chiedere al Titolare l&lsquo;accesso ai Vostri i dati personali ed
                 alle informazioni relative agli stessi (art. 15 GDPR); la
-                rettifica dei dati inesatti o l'integrazione di quelli
+                rettifica dei dati inesatti o l&lsquo;integrazione di quelli
                 incompleti (art. 16 GDPR); la cancellazione (c.d. “diritto
-                all’oblio”) dei dati personali che Vi riguardano (al verificarsi
-                di una delle condizioni indicate nell'art. 17, paragrafo 1,
+                all&lsquo;oblio”) dei dati personali che Vi riguardano (al verificarsi
+                di una delle condizioni indicate nell&lsquo;art. 17, paragrafo 1,
                 GDPR, e nel rispetto delle eccezioni previste nel paragrafo 3
                 dello stesso articolo); la limitazione del trattamento dei Suoi
                 dati personali (al ricorrere di una delle ipotesi indicate
-                nell'art. 18, paragrafo 1 GDPR);{" "}
+                nell&lsquo;art. 18, paragrafo 1 GDPR);
               </li>
 
               <li>
-                richiedere ed ottenere dal Titolare – nelle ipotesi in cui la
+                richiedere ed ottenere dal Titolare - nelle ipotesi in cui la
                 base giuridica del trattamento sia il contratto o il consenso, e
-                lo stesso sia effettuato con mezzi automatizzati – i Vostri dati
+                lo stesso sia effettuato con mezzi automatizzati - i Vostri dati
                 personali in un formato strutturato e leggibile da dispositivo
                 automatico, anche al fine di comunicare tali dati ad un altro
                 titolare del trattamento (c.d. diritto alla portabilità dei dati
@@ -232,7 +232,7 @@ export default function CodeOfConduct() {
               <li>
                 opporsi in qualsiasi momento al trattamento dei Vostri i dati
                 personali al ricorrere di situazioni particolari che Vi
-                riguardano (art. 21 GDPR);{" "}
+                riguardano (art. 21 GDPR);
               </li>
 
               <li>
@@ -241,21 +241,21 @@ export default function CodeOfConduct() {
                 basato sul Vostro consenso per una o più specifiche finalità e
                 riguardi dati personali comuni (ad esempio data e luogo di
                 nascita o luogo di residenza), o particolari categorie di dati
-                (ad esempio dati che rivelano l’origine razziale, opinioni
+                (ad esempio dati che rivelano l&lsquo;origine razziale, opinioni
                 politiche, convinzioni religiose, lo stato di salute o la vita
                 sessuale). Il trattamento basato sul consenso ed effettuato
                 antecedentemente alla revoca dello stesso conserva, comunque, la
-                sua liceità;{" "}
+                sua liceità;
               </li>
 
               <li>
-                proporre reclamo a un'autorità di controllo (Autorità Garante
-                per la protezione dei dati personali – www.garanteprivacy.it).
+                proporre reclamo a un&lsquo;autorità di controllo (Autorità Garante
+                per la protezione dei dati personali - www.garanteprivacy.it).
               </li>
             </ul>
-            L’esercizio dei suindicati diritti può essere esercitato mediante
+            L&lsquo;esercizio dei suindicati diritti può essere esercitato mediante
             comunicazione scritta al Titolare del trattamento da inviare via
-            mail all’indirizzo di seguito indicato:
+            mail all&lsquo;indirizzo di seguito indicato:
             schrodinger.hat.show@gmail.com
           </p>
         </div>
