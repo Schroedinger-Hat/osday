598abada90dfd517d061fd740b086dc55d99fa17
Miki Lombardi <miki.lombi@gmail.com>
2023-03-24 07:13:17 +0100

    fix: props

--- components/Main.tsx
+++ components/Main.tsx
@@ -23,7 +23,7 @@ export default function Main() {
         showTicketBtn
         showTicketAvailability
       />
-      <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="100%" height="650" type="text/html" src="https://www.youtube.com/embed/1tnQLkcToIQ?autoplay=1&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0"></iframe>
+      <iframe frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"width="100%" height="650" type="text/html" src="https://www.youtube.com/embed/1tnQLkcToIQ?autoplay=1&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0"></iframe>
     </div>
   );
 }
