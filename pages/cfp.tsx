b4f8da1e4ea3278c67073b6922e4ba40fe53b5c8
Lorenzo Pieri <lorenzo.pieri@birdie.care>
2023-10-31 10:01:47 +0100

    feat: publish tickets, cfp and css changes

--- pages/cfp.tsx
+++ pages/cfp.tsx
@@ -1,42 +1,42 @@
-import CfpCard from '../components/CfpCard';
-import Hero from '../components/Hero';
-import { cfpTypes } from '../constants';
-import { useTranslations } from 'next-intl';
+import CfpCard from "../components/CfpCard";
+import Hero from "../components/Hero";
+import { cfpTypes } from "../constants";
+import { useTranslations } from "next-intl";
 
 export async function getStaticProps({ locale }: { locale: any }) {
   return {
     props: {
       metas: {
-        title: 'CFP, Open Source Day 2023 - Florence',
+        title: "CFP, Open Source Day 2024 - Florence",
         description:
-          'Open Source Day 2023 coming on the 24th of March 2023. Stay tuned on our social'
+          "Open Source Day 2024 coming on the 7th and 8th of March 2024. Stay tuned on our social",
       },
-      messages: (await import(`../public/locales/${locale}.json`)).default
-    }
+      messages: (await import(`../public/locales/${locale}.json`)).default,
+    },
   };
 }
 
 export default function CFP() {
-  const t = useTranslations('Cfp');
+  const t = useTranslations("Cfp");
   return (
     <>
       <div className="container">
         <Hero
-          title="Open Source Day 2023"
+          title="Open Source Day 2024"
           subtitle="Call for papers"
-          description={t('description')}
+          description={t("description")}
           originals={false}
           secondaryCta={{
-            text: t('secondarycta_text'),
-            link: 'mailto:osday@schrodinger-hat.it'
+            text: t("secondarycta_text"),
+            link: "mailto:osday@schrodinger-hat.it",
           }}
           showTicketBtn
         />
 
         <section className="after_main">
-          <h2>{t('description_1')}</h2>
+          <h2>{t("description_1")}</h2>
           <h3>
-            {t('description_2')}{' '}
+            {t("description_2")}{" "}
             <a href="https://www.schrodinger-hat.it/">Schrodingers Hat</a>
           </h3>
           <div className="after_main_container">
