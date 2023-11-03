93c0f6a72e12c551653515b3652e417dfa2ec3b8
Lorenzo Pieri <79904453+404answernotfound@users.noreply.github.com>
2023-11-03 19:39:23 +0100

    feat/videos on homepage (#333)
    * feat/videos on homepage
    
    * style(globals): reduce class boilerplate
    
    ---------
    
    Co-authored-by: Patrick Raedler <code@readpato.dev>

--- components/TalkCard.tsx
+++ components/TalkCard.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { TTalkCard } from '../constants';
+import React from "react";
+import { TTalkCard } from "../constants";
 
-const TalkCard = ({ description, link }: TTalkCard) => {
+const TalkCard = ({ description, link, author }: TTalkCard) => {
   return (
     <div className={`talk_card`}>
-      {link ? (
-        <p>
-          <a href={link} target="_blank" rel="noreferrer">{description}</a>
-        </p>
-      ) : (
-        <p>{description} </p>
-      )}
+      <div className={`talk_card_inner ${author}`}>
+        {link ? (
+          <p>
+            <a href={link} target="_blank" rel="noreferrer">
+              {description}
+            </a>
+          </p>
+        ) : (
+          <p>{description} </p>
+        )}
+      </div>
     </div>
   );
 };
