52e7c356bd15b7ba47e288eade6974f527af1776
Miki Lombardi <miki.lombi@gmail.com>
2024-03-05 17:23:44 +0000

    feat(507): remove last edition talks, add agenda link

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,27 @@ import TalkCard from "../components/TalkCard";
 import { TTalkCard, talks2023 } from "../constants";
 import Main from "../components/Main";
 import SponsorTable from "../components/SponsorTable";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Main />
-      <section className="talks_2023">
+      <div style={{marginBottom: '4em'}} className="container-stream-link">
+        <h2 className="sponsors_thanks">We are live!</h2>
+        <Link href="/agenda" className="button">
+          Click to discover the agenda!
+        </Link>
+      </div>
+      <SponsorTable isVisible></SponsorTable>
+      {/* <section className="talks_2023">
         <h1>Last edition talks:</h1>
         <div className="talks_container">
           {talks2023.map((talk: TTalkCard) => {
               return <TalkCard key={talk.id} {...talk} />;
             })}
         </div>
-        <SponsorTable isVisible></SponsorTable>
-      </section>
+      </section> */}
     </>
   );
 }
