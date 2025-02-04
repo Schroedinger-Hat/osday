e5383d4dc6ae009505ab621595f707de59a2be61
Patrick Raedler <code@readpato.dev>
2025-02-04 17:04:51 +0100

    refactor(Layout): hide sponsors on home path

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -64,7 +64,7 @@ export default function Layout({
     }, [router.events]);
 
     const areSponsorVisible = () => {
-        return router.asPath === '/sponsor' || router.asPath === '/';
+        return router.asPath === '/sponsor';
     };
 
     return (
@@ -83,7 +83,7 @@ export default function Layout({
                 <meta property="og:url" content={metas.url ? metas.url : 'https://2025.osday.dev'} />
                 <meta property="og:description" content={metas.description ? metas.description : "Open Source Day 2025 coming on 21 March 2025. Stay tuned on our socials"} />
                 <meta property="og:site_name" content="Open Source Day 2025" />
-                
+
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:image" content={metas.image ? metas.image : 'https://osday.dev/intro-2025.png'} />
                 <meta name="twitter:image:alt" content="Open Source Day 2025" />
