f89bb36d02cca6c5211ec74c04cfda7dfdf61407
Mattia Lobertini <LobeTia@users.noreply.github.com>
2025-02-28 12:50:42 +0300

    Sitemap fix (#599)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: "/sanity-cms/",
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
