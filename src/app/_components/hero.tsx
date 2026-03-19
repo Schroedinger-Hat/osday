6ba0dc471fa7e553dc296973c386ccd4abe0e629
Cristiano Casella <cristiano.casella@schroedinger-hat.org>
2026-03-19 16:16:18 +0100

    fix: Fixed typo in homepage

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -66,7 +66,7 @@ export default function Hero() {
             </div>
 
             <Heading level={1} huge>
-              FOR WHO LOVES
+              FOR THOSE WHO LOVES
               <br />
               OPEN SOURCE
             </Heading>
