ea2b195c0e67853078fe36d8d5a087efabaac2e5
Marco Giuliotti <marco.giuliotti@gmail.com>
2026-04-03 14:49:51 +0200

    Set hover state for talk boxes

--- src/components/molecules/talks-table.tsx
+++ src/components/molecules/talks-table.tsx
@@ -73,7 +73,7 @@ function TalkCell({ item }: { item: TimelineItem }) {
   return (
     <Link
       href={`/schedule/${item._id}`}
-      className="block overflow-hidden rounded border-[0.5px] border-fiery-red bg-white transition-opacity hover:opacity-80"
+      className="block overflow-hidden rounded border-[0.5px] border-fiery-red bg-white transition-all hover:border-fiery-red/50 hover:opacity-80 hover:bg-red-100"
     >
       <div className="flex min-h-24 flex-col gap-1 border-l-4 border-fiery-red px-4 py-2.5">
         <p className="text-lg font-bold leading-snug tracking-tight text-black">
